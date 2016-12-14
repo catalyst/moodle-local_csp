14e2642d89a074ad5f7b9103c10621e9139f32ad
Suan Kan <suankan@catalyst-au.net>
2016-12-15 10:28:54 +1100

    #2: Make activities only if CSP JSON report was decoded successfully.

--- collector.php
+++ collector.php
@@ -30,32 +30,36 @@ $cspreport = json_decode($inputjson, true)['csp-report'];
 require_once(__DIR__ . '/../../config.php');
 global $DB;
 
-$existingrecord = $DB->get_record('local_csp', array(
-    'documenturi' => $cspreport['document-uri'],
-    'blockeduri' => $cspreport['blocked-uri'],
-    'violateddirective' => $cspreport['violated-directive'],
-));
+if ($cspreport) {
+    $existingrecord = $DB->get_record('local_csp', array(
+        'documenturi' => $cspreport['document-uri'],
+        'blockeduri' => $cspreport['blocked-uri'],
+        'violateddirective' => $cspreport['violated-directive'],
+    ));
 
-$dataobject = new stdClass();
+    $dataobject = new stdClass();
 
-try {
-    if ($existingrecord) {
-        // Just increment failcounter of the existing record.
-        $dataobject->id = $existingrecord->id;
-        $dataobject->failcounter = $existingrecord->failcounter + 1;
-        $dataobject->timeupdated = time();
-        $DB->update_record('local_csp', $dataobject);
-        echo 'Repeated CSP violation, failcounter incremented.';
-    } else {
-        // Insert a new record.
-        $dataobject->documenturi = $cspreport['document-uri'];
-        $dataobject->blockeduri = $cspreport['blocked-uri'];
-        $dataobject->violateddirective = $cspreport['violated-directive'];
-        $dataobject->timecreated = time();
-        $DB->insert_record('local_csp', $dataobject);
-        echo 'New CSP violation recorded.';
+    try {
+        if ($existingrecord) {
+            // Just increment failcounter of the existing record.
+            $dataobject->id = $existingrecord->id;
+            $dataobject->failcounter = $existingrecord->failcounter + 1;
+            $dataobject->timeupdated = time();
+            $DB->update_record('local_csp', $dataobject);
+            echo 'Repeated CSP violation, failcounter incremented.';
+        } else {
+            // Insert a new record.
+            $dataobject->documenturi = $cspreport['document-uri'];
+            $dataobject->blockeduri = $cspreport['blocked-uri'];
+            $dataobject->violateddirective = $cspreport['violated-directive'];
+            $dataobject->timecreated = time();
+            $DB->insert_record('local_csp', $dataobject);
+            echo 'New CSP violation recorded.';
+        }
+    } catch (dml_exception $exception) {
+        echo 'There was a problem with recording CSP report to Moodle database.';
+        throw $exception;
     }
-} catch (dml_exception $exception) {
-    echo 'There was a problem with recording CSP report to Moodle database.';
-    throw $exception;
+} else {
+    echo "There was a problem with decoding JSON report.";
 }
