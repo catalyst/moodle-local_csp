6eb786ae68758c8ca03e2df89587709e4588f104
Brendan Heywood <brendan@catalyst-au.net>
2020-10-26 10:05:54 +1100

    Do not touch the session at all when collecting #53

--- collector.php
+++ collector.php
@@ -23,6 +23,7 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+define('NO_MOODLE_COOKIES', true);
 // @codingStandardsIgnoreStart
 require_once(__DIR__ . '/../../config.php');
 // @codingStandardsIgnoreEnd
